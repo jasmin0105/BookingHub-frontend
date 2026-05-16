7264f0d673d3ad9e9fdcb03faa5c024098e36e4f
zhasmin <yusu1pova@gmail.com>
2026-05-16 18:25:44 +0600

    Fix Nomad and Guides API URL for production

--- src/views/GuidesView.vue
+++ src/views/GuidesView.vue
@@ -102,7 +102,7 @@
 import { ref, onMounted } from "vue"
 import axios from "axios"
 
-const api = axios.create({ baseURL: "http://localhost:8001/api" })
+const api = axios.create({ baseURL: "https://bookinghub-a69i.onrender.com/api" })
 const guides = ref([])
 const loading = ref(true)
 const selectedGuide = ref(null)
--- src/views/NomadView.vue
+++ src/views/NomadView.vue
@@ -119,7 +119,7 @@
 <script setup>
 import { ref, onMounted } from "vue"
 import axios from "axios"
-const api = axios.create({ baseURL: "http://localhost:8001/api" })
+const api = axios.create({ baseURL: "https://bookinghub-a69i.onrender.com/api" })
 
 const packages = ref([])
 const loading = ref(true)
