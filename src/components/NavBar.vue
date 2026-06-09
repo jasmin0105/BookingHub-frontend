0c0294c7fc2e1a63ac09730deac12fe96f0589a4
zhasmin <yusu1pova@gmail.com>
2026-06-09 14:24:53 +0600

    Fix NavBar translations

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -39,7 +39,7 @@ cd ~/Desktop/BookingHub_frontend cat > src/components/NavBar.vue << 'NAVEOF'
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
             />
           </svg>
-          Home
+          {{ $t("nav.home") }}
         </RouterLink>
         <RouterLink
           to="/hotels"
@@ -59,7 +59,7 @@ cd ~/Desktop/BookingHub_frontend cat > src/components/NavBar.vue << 'NAVEOF'
               d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
             />
           </svg>
-          Hotels
+          {{ $t("nav.hotels") }}
         </RouterLink>
         <RouterLink
           to="/restaurants"
@@ -79,7 +79,7 @@ cd ~/Desktop/BookingHub_frontend cat > src/components/NavBar.vue << 'NAVEOF'
               d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
             />
           </svg>
-          Restaurants
+          {{ $t("nav.restaurants") }}
         </RouterLink>
         <RouterLink
           to="/events"
@@ -99,7 +99,7 @@ cd ~/Desktop/BookingHub_frontend cat > src/components/NavBar.vue << 'NAVEOF'
               d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
           </svg>
-          Events
+          {{ $t("nav.events") }}
         </RouterLink>
         <RouterLink
           to="/tours"
